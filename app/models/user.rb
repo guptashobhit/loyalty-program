0adac65fbb0a1217fae0f731bcc893088e949398
Shobhit <shobhit@poshvine.com>
2022-05-30 23:20:22 +0530

    encrypt email and mobile no.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  encrypts :email, :mobile, deterministic: true #AES256 encryption of PII data
   has_many :spends
   has_many :points
+
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true }
+  validates :mobile, presence: true, uniqueness: true
 end
